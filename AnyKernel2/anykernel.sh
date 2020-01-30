9c79e61c8cb98cb25a13c3a8f4a8dde3e1027538
dhruv <dhruvgera61@gmail.com>
2020-01-30 03:51:54 +0000

    Cygnus-Ify

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,34 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Dungphp
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=hydrogen
+device.name2=o
+device.name3=s
+device.name4=helium
+device.name5=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel install
+dump_boot;
+write_boot;
+
+## end install
+
